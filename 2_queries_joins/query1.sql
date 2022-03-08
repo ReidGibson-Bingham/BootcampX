49570ef664133af4a47251c5150666a10f2f60f6
P4ulH1ll <jsr3b0rn@gmail.com>
2022-03-08 02:03:23 +0000

    the HAVING clause is like WHERE but it only works on aggregated data. ie. data from the COUNT clause

--- 2_queries_joins/query1.sql
+++ 2_queries_joins/query1.sql
@@ -1,7 +1,9 @@
 SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
-JOIN students ON students.id = student_id
+JOIN students ON students.id = student_id 
+-- ^ this is joining students.id onto assignment_submissions.student_id
 JOIN cohorts ON cohorts.id = cohort_id
+
 WHERE cohorts.name = 'FEB12';
 
 
